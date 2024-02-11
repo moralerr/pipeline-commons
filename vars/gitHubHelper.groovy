4d0f0708cf5ab9067de4f4351b046c839ba857aa
moralerr <moralerrusc@gmail.com>
2024-02-11 15:02:30 -0500

    remove class from github helper library

--- vars/gitHubHelper.groovy
+++ vars/gitHubHelper.groovy
@@ -1,46 +1,34 @@
 // vars/githubHelper.groovy
 
-class GitHubHelper {
+def getPullRequestDetails(String apiUrl, String accessToken, String owner, String repo, int pullRequestId) {
+    def response = httpRequest(
+        url: "${apiUrl}/repos/${owner}/${repo}/pulls/${pullRequestId}",
+        httpMode: 'GET',
+        headers: [
+            Authorization: "Bearer ${accessToken}"
+        ]
+    )
 
-    String apiUrl
-    String accessToken
-
-    GitHubHelper(String apiUrl, String accessToken) {
-        this.apiUrl = apiUrl
-        this.accessToken = accessToken
+    if (response.status == 200) {
+        return readJSON text: response.content
+    } else {
+        error "Failed to fetch pull request details: ${response.status} - ${response.content}"
     }
+}
 
-    def getPullRequestDetails(String owner, String repo, int pullRequestId) {
-        def response = httpRequest(
-            url: "${apiUrl}/repos/${owner}/${repo}/pulls/${pullRequestId}",
-            httpMode: 'GET',
-            headers: [
-                Authorization: "Bearer ${accessToken}"
-            ]
-        )
+def mergePullRequest(String apiUrl, String accessToken, String owner, String repo, int pullRequestId) {
+    def response = httpRequest(
+        url: "${apiUrl}/repos/${owner}/${repo}/pulls/${pullRequestId}/merge",
+        httpMode: 'PUT',
+        headers: [
+            Authorization: "Bearer ${accessToken}"
+        ],
+        requestBody: '{"merge_method": "merge"}'
+    )
 
-        if (response.status == 200) {
-            return readJSON(response.content)
-        } else {
-            error "Failed to fetch pull request details: ${response.status} - ${response.content}."
-        }
+    if (response.status == 200 || response.status == 201) {
+        return true
+    } else {
+        error "Failed to merge pull request: ${response.status} - ${response.content}"
     }
-
-    def mergePullRequest(String owner, String repo, int pullRequestId) {
-        def response = httpRequest(
-            url: "${apiUrl}/repos/${owner}/${repo}/pulls/${pullRequestId}/merge",
-            httpMode: 'PUT',
-            headers: [
-                Authorization: "Bearer ${accessToken}"
-            ],
-            requestBody: '{"merge_method": "merge"}'
-        )
-
-        if (response.status == 200 || response.status == 201) {
-            return true
-        } else {
-            error "Failed to merge pull request: ${response.status} - ${response.content}"
-        }
-    }
-
 }
